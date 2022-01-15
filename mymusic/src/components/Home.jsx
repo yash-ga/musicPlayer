011d28b6e8977f61ac36863b2a3214d9c519985d
Yash Garg <yash_fw10_103@masai.school>
2022-01-15 13:15:05 +0530

    home

--- mymusic/src/components/Home.jsx
+++ mymusic/src/components/Home.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react"
+
 export const Home=()=>{
+    const [arr,setarr]=useState([])
+    const   getdata=async()=>{
+        var res=await fetch("http://localhost:3001/Album")
+        var data=await res.json()
+        console.log(data);
+        setarr(data)
+    }
+    useEffect(()=>{
+        
+        getdata()
+    },[])
     return(
+
         <div>
             <h1>this is home page</h1>
+           
+            {
+                arr.map((e,ind)=>{
+                    return(
+                        <div>
+
+                            <div className="artist" key={ind}>{e.artist}</div>
+                            <div className="song">{e.song.map((s)=>{
+                                return(
+                                    <div>{s.title}</div>
+                                )
+                            })}</div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
